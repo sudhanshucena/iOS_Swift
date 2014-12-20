0527fcfc0c2d1fc39f57875c8786a7063cee9f0c
sudhanshucena <s1459349@sms.ed.ac.uk>
2014-12-20 20:47:25 +0000

    ToDoList App
    Basic TODO list app main implementation of TableViews

--- ToDoList/ToDoList/FirstViewController.swift
+++ ToDoList/ToDoList/FirstViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+var toDoItems:[String] = []
+class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
-class FirstViewController: UIViewController {
-
+    @IBOutlet weak var tasksTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewWillAppear(animated: Bool) {
+        
+        if var storedToDoItems : AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("toDoItems"){
+            toDoItems=[]
+            for var index=0 ; index < storedToDoItems.count; index++             {
+                toDoItems.append(storedToDoItems[index] as NSString)
+                }
+        }
+        tasksTable.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,30 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    
+    {
+        return toDoItems.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoItems[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+            
+        toDoItems.removeAtIndex(indexPath.row)
+           
+        let fixedToDoItem = toDoItems
+        NSUserDefaults.standardUserDefaults().setObject(fixedToDoItem, forKey: "toDoItems")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        tasksTable.reloadData()
 
+        }
 }
-
+}
--- ToDoList/ToDoList/SecondViewController.swift
+++ ToDoList/ToDoList/SecondViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-    var toDoItems = ["eat","drink","sleep","repeat"]
+class SecondViewController: UIViewController,UITextFieldDelegate {
+    //note syntax to
+   
+    
     @IBOutlet weak var enterItemText: UITextField!
     
     @IBAction func addItem(sender: UIButton) {
+        if (enterItemText.text.isEmpty) {
+            let alert = UIAlertView()
+            alert.title = "No Text"
+            alert.message = "Please Enter Text In The Box"
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+            
+        }
+        else{
+           enterItemText.clearsOnBeginEditing = true
+        toDoItems.append(enterItemText.text)
+        let fixedToDoItem = toDoItems
+        NSUserDefaults.standardUserDefaults().setObject(fixedToDoItem, forKey: "toDoItems")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        self.view.endEditing(true)
+            
+        }
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +43,14 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func textFieldShouldReturn(enterItemText: UITextField!) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        enterItemText.resignFirstResponder()
+        return true;
+    }
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
 
 }
 
